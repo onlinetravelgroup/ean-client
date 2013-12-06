5f3f8137b1172626808f731d707d1d08932dfcdb
Ryan Hughes <rhughes@hotels.com.au>
2013-12-06 15:29:44 +1100

    Add helper method to collect parameters for PostReservation
    * Fix getRoom method on RoomAvailability results using the options parameter

--- src/Otg/Ean/Hotel/Model/RoomAvailabilityModel.php
+++ src/Otg/Ean/Hotel/Model/RoomAvailabilityModel.php
@@ -18,12 +18,43 @@ class RoomAvailabilityModel extends GuzzleModel
     public function getRoom($roomTypeCode, $rateCode)
     {
         foreach ($this->data['Rooms'] as $room) {
-            if ($room['rateCode'] == $rateCode && $room['roomTypeCode'] == $roomTypeCode) {
+            $roomCode = isset($room['roomTypeCode']) ? $room['roomTypeCode'] : $room['RoomType']['roomCode'];
+            if ($room['rateCode'] == $rateCode && $roomCode == $roomTypeCode) {
                 return $room;
             }
         }
 
         // sold out responses are caught before this point.
         throw new \UnexpectedValueException('The selected room does not exist');
+    }
+
+    /**
+     * Returns parameters from a RoomAvailability response used in a PostReservation request
+     *
+     * @param  string $roomTypeCode roomTypeCode of the room being booked
+     * @param  string $rateCode     rateCode of the room being booked
+     * @return array
+     */
+    public function getReservationParameters($roomTypeCode, $rateCode)
+    {
+        $room = $this->getRoom($roomTypeCode, $rateCode);
+
+        if ($room['supplierType'] == 'E') {
+            $chargeableRate = $room['RateInfos'][0]['ChargeableRateInfo']['total'];
+        } else {
+            $chargeableRate = $room['RateInfos'][0]['ChargeableRateInfo']['maxNightlyRate'];
+        }
+
+        return array(
+            'hotelId' => $this->data['hotelId'],
+            'arrivalDate' => $this->data['arrivalDate'],
+            'departureDate' => $this->data['departureDate'],
+            'supplierType' => $room['supplierType'],
+            'rateKey' => $room['RateInfos'][0]['RoomGroup'][0]['rateKey'],
+            'roomTypeCode' => isset($room['roomTypeCode']) ? $room['roomTypeCode'] : $room['RoomType']['roomCode'],
+            'rateCode' => $room['rateCode'],
+            'chargeableRate' => $chargeableRate,
+            'currencyCode' => $room['RateInfos'][0]['ChargeableRateInfo']['currencyCode']
+        );
     }
 }
